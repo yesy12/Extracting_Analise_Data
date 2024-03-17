455bd68b6cc6691307741b1255bd074a258f3a46
alison <alisonvieira.av29@gmail.com>
2024-03-17 18:03:08 -0300

    Requirements txt

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 
 from time import sleep
 
-# from package.multiClass.Review import Review
+from package.multiClass.Review import Review
 
 # link = "https://steamcommunity.com/app/678950/reviews/?browsefilter=toprated&snr=1_5_100010_"
 # infos = []
