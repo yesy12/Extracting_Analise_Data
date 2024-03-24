8ca0ffb5e1719568fb44057409b294bf7d1372c8
alison <alisonvieira.av29@gmail.com>
2024-03-24 15:30:35 -0300

    Resolving bugs Formatter Date 1

--- package/multiClass/PlayerAboutInformation/Comments/Comments.py
+++ package/multiClass/PlayerAboutInformation/Comments/Comments.py
@@ -6,20 +6,25 @@ class Commnents:
     def __init__(self) -> None:
         pass
         
-    def formaterDate(self, comentDate) -> str:
+    def formaterDate(self, comentDate):
         comentDate = splitSpecific(r"às \d+:\d+", comentDate)[0]
         if(len(comentDate) == 13):
             comentDate = splitSpecific(r"/", comentDate)
 
         elif(len(comentDate) == 11):
             comentDate = splitSpecific(" de ", comentDate)
-        
-        comentDate[1] = getIndexsFromMonthCommnets(comentDate[1][0:4])
-        comentDate[0] = int(comentDate[0])
+            comentDate[1] = comentDate[1][0:4]
+                    
+        if type(comentDate) == str:
+            comentDate = splitSpecific("/", comentDate)        
+
+        comentDate[1] = getIndexsFromMonthCommnets(comentDate[1])
+
         if len(comentDate) == 3:
             comentDate[2] = int(comentDate[2])
+        else:
+            comentDate = addYearDatePublish(comentDate)
 
-        comentDate = addYearDatePublish(comentDate)
         return f"{comentDate[0]}-{comentDate[1]}-{comentDate[2]}"
 
     def formaterComment(self, commentText) -> str:
