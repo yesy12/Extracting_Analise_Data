89436ef3b8618416fcfe037475dd91db81f2c75f
alison <alisonvieira.av29@gmail.com>
2024-03-24 16:52:52 -0300

    Moving

--- Extracting Data/start.py
+++ Extracting Data/start.py
@@ -3,13 +3,13 @@ from time import sleep
 
 from package.multiClass.Review import Review
 
-link = "https://steamcommunity.com/app/678950/reviews/?browsefilter=toprated&snr=1_5_100010_"
+# link = "https://steamcommunity.com/app/678950/reviews/?browsefilter=toprated&snr=1_5_100010_"
+link = "https://steamcommunity.com/app/1310410/reviews/?browsefilter=toprated&snr=1_5_100010_"
 infos = []
 
 newReview = Review()
 newReview.getLink(link)
 newReview.gameActual()
-newReview.saveGameTitle()
 
 for i in range(1,1000):
     sleep(1)
