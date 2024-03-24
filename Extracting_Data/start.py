ebfb467ca634257921d3935ccb91535a30f68fbf
alison <alisonvieira.av29@gmail.com>
2024-03-24 18:46:05 -0300

    get links from file

--- Extracting_Data/start.py
+++ Extracting_Data/start.py
@@ -3,37 +3,35 @@ from time import sleep
 
 from package.multiClass.Review import Review
 from os import getcwd
-# # link = "https://steamcommunity.com/app/678950/reviews/?browsefilter=toprated&snr=1_5_100010_"
-link = "https://store.steampowered.com/app/2420110/Horizon_Forbidden_West__Edio_Completa/"
-# diretorioAtual = f"{getcwd()}/Extracting_Data/games.txt"
-# print(diretorioAtual)
-# with open(diretorioAtual, "r", encoding="utf-8") as lines:
-#     for line in lines.readlines():
-#         print(line)
-
 
 newReview = Review()
-newReview.getLink(link)
-newReview.ageCheck()
-newReview.getAllReviews()
-sleep(2)
-newReview.gameActual()
 
-for i in range(1,1000):
-    sleep(1)
-    newReview.setPageRow(i)
-    result = newReview.exitOnThisGame(120)
+diretorioAtual = f"{getcwd()}/Extracting_Data/games.txt"
 
-    if result == True:
-        print("Proximo")
-        break   
+with open(diretorioAtual, "r", encoding="utf-8") as lines:
+    for line in lines.readlines():    
+        print(f"Jogo: {line}")      
+        newReview.getLink(line)
+        newReview.ageCheck()
+        newReview.getAllReviews()
+        sleep(2)
+        newReview.gameActual()
 
-    newReview.getGeral()
+        for i in range(1,10000):
+            sleep(1)
+            newReview.setPageRow(i)
+            result = newReview.exitOnThisGame(1000)
 
-    lastHeight = newReview.getScrollHeight()
-    newReview.scroll()
-    newScrollHeight = newReview.getScrollHeight()
+            if result == True:
+                print("Proximo")
+                break   
 
-    if lastHeight == newScrollHeight:
-        break
+            newReview.getGeral()
+
+            lastHeight = newReview.getScrollHeight()
+            newReview.scroll()
+            newScrollHeight = newReview.getScrollHeight()
+
+            if lastHeight == newScrollHeight:
+                break
 
